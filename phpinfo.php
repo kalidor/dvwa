5e2fb4c45c52aadd8695e19733bb7f0952e34cff
kalidor <kalidor@unixed.fr>
2023-03-14 16:19:39 +0100

    plop

--- phpinfo.php
+++ phpinfo.php
@@ -1,5 +1,9 @@
 <?php
-/*comment*/
+/*csdjjsdk
+ * fjlaksjdfkasldjf
+ * klasdj
+ * fklj
+ * omment*/
 define( 'DVWA_WEB_PAGE_TO_ROOT', '' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
